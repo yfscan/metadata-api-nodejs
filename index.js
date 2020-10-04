94ae7b7fb4df3aad34367718323e6592c48cf26c
cuswap <70313860+cuswap@users.noreply.github.com>
2020-10-04 13:56:13 +0800

    Updated api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
-const moment = require('moment')
 const { HOST } = require('./src/constants')
-const db = require('./src/database')
+const fs = require('fs');
+let db = JSON.parse(fs.readFileSync('./src/database.json'));
 
 const PORT = process.env.PORT || 5000
 
@@ -15,24 +15,24 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.send('Get ready for OpenSea!');
+  res.send('Get ready for yfLambda!');
 })
 
-app.get('/api/token/:token_id', function(req, res) {
+app.get('/api/:token_id', function(req, res) {
   const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
+  const attributes = db[tokenId]
   const data = {
-    'name': person.name,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
-    },
-    'image': `${HOST}/images/${tokenId}.png`
+    'image': `${HOST}/images/` + attributes["img"],
+    'attributes': [
+      {
+        'trait_type': 'amount',
+        'value': attributes['amount']
+      },
+      {
+        'trait_type': 'minter',
+        'value': attributes['minter']
+      }
+    ]
   }
   res.send(data)
 })
@@ -41,15 +41,3 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
 
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const HOST = 'https://birthstamps.herokuapp.com'
+const HOST = 'https://yscan.herokuapp.com'
 
 module.exports = {
   HOST
